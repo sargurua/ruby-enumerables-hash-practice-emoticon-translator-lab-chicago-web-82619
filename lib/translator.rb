2479871dcff939de2f8407d60e94596c794f710a
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-13 18:52:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,9 +9,8 @@ def load_library(path)
   file = YAML.load_file(path)
   file.each do |meaning, emoticon|
     jap, eng = emoticon
-    emoticons["get_meaning"][jap] = emoticon
-    emoticon["get_meaning"][eng] = jap
-    
+    emoticons["get_meaning"][jap] = emoticon[0]
+    emoticon["get_meaning"][eng] = emoticons[1]
   end
   emoticons
 end
